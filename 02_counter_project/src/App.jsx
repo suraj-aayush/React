1fe9904089e4c45e92a671148b819411b22a9014
Aayush Kumar <aayushagrawal296@gmail.com>
2024-04-09 02:18:37 +0530

    counter project

--- 02_counter_project/src/App.jsx
+++ 02_counter_project/src/App.jsx
@@ -4,21 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-let counter = 15 
 
-const addValue = () =>{
-  counter++; 
-  console.log(`value added ${Math.random()}`);
-}
+  let [counter, setCount] = useState(10)
+
+  const increase = () =>{
+    setCount(counter +1)
+    console.log("clicked " + Math.random());
+  }
+
+  const decrease = () =>{
+    setCount(counter - 1)
+    console.log(" decreased " + Math.random());
+  }
+
 
   return (
     <>
-      <h1>02 counter project</h1>
-      <h2>counter value :5</h2>
+      
+    <h2>Counter Project</h2>
+    <h4>Counter Value : {counter} </h4>
 
-      <button onClick={addValue}>Add</button>
-      <br />
-      <button>Sub</button>
+    <button
+      onClick={increase}
+    > Increase </button>
+    <br /> 
+    <button
+      onClick={decrease}
+    > Decrease </button>
+
     </>
   )
 }
