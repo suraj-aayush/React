ddd0747fb6287f81ecd094b210bc29f9cc922fe5
Aayush Kumar <aayushagrawal296@gmail.com>
2024-05-06 22:50:22 +0530

    res

--- 06_Currency_Converter/src/custom_hooks/useCurrencyInfo.js
+++ 06_Currency_Converter/src/custom_hooks/useCurrencyInfo.js
@@ -3,6 +3,6 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(){
     useEffect( ()=>{
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then( (response)=> response.json() )
+        .then( (res)=> res.json() )
     }, [] )
 } 
